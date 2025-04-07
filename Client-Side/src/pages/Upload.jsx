71e71aeb3ab06822bc6964807ff2c989241e38ca
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-04-07 12:51:58 +0530

    added changes

--- Client-Side/src/pages/Upload.jsx
+++ Client-Side/src/pages/Upload.jsx
@@ -1,29 +1,74 @@
-import React from 'react';
-import './Stylesheets/upload.css';
-
+import React, { useEffect } from "react";
+import "./Stylesheets/upload.css";
 
 function Upload() {
-    const [files,setFiles] = React.useState([]);
-    function handlefiles(e){
-        const newFiles = e.target.files
-        setFiles((prev)=>[...prev,...newFiles])
+    // variables
+
+    // setting States
+    const [files, setFiles] = React.useState([]);
+
+    // Refs
+    const inputRef = React.useRef(null);
+
+    // Functions
+    function handlefiles(e) {
+        const newFiles = e.target.files;
+        setFiles((prev) => [...prev, ...newFiles]);
     }
-    console.log(files)
-    
+    function clearFiles() {
+        if (files.length > 0) {
+            setFiles([]);
+        } else {
+            console.log("No files to clear");
+        }
+    }
+    function deleteFile(index){
+        setFiles((prev) => prev.filter((_, i) => i !== index));
+    }
+
+    // effects
+    useEffect(() => {
+        console.log(files);
+        inputRef.current.value = null;
+    }, [files]);
+
     return (
-        <div className='upload-container'>
-            <div className='upload-upload-div'>
-                <input type="file" multiple onChange={(e)=>handlefiles(e)} />
+        <div className="upload-container">
+            <div className="upload-upload-div">
+                <input
+                    type="file"
+                    multiple
+                    accept="application/pdf"
+                    onChange={(e) => handlefiles(e)}
+                    ref={inputRef}
+                />
             </div>
+
             <div>
-                {
-                    
-                }
+                {files.map((file, i) => {
+                    const fileURL = URL.createObjectURL(file);
+                    return (
+                        <div className="preview" key={i}>
+                            <embed
+                                key={i}
+                                src={fileURL}
+                                type="application/pdf"
+                                width="200"
+                                height="150"
+                            />
+                            <p>{file.name}</p>
+                            <button onClick={()=>deleteFile(i)}>Delete</button>
+                        </div>
+                    );
+                })}
             </div>
+
             <div className="upload-btn-holder">
-                <button className='upload-btn'>Clear files</button>
-                <button className='upload-btn'>Add More files</button>
-                <button className='upload-btn'>Manage files</button>
+                <button className="upload-btn" onClick={clearFiles}>
+                    Clear files
+                </button>
+                <button>Generate Flash Cards</button>
+                <button>Generate Questionnaire</button>
             </div>
         </div>
     );
