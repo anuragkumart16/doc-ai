ad3c1650ae294b2c66d6b15285a0ce14202345f8
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-04-07 09:10:15 +0530

    added refresh token to user model

--- Server-Side/src/models/user.model.js
+++ Server-Side/src/models/user.model.js
@@ -3,20 +3,28 @@ import mongoose,{Schema} from "mongoose";
 const userSchema = new Schema({
     firstname: {
         type: String,
-        required: true,
+        required: [true, "firstname is required :src/modals/user.models.js"],
+        trim: true
     },
     lastname:{
         types: String,
-        required: true
+        required: [true, "lastname is required :src/modals/user.models.js"],
+        trim: true
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Password is required :src/modals/user.models.js"],
+        trim: true
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "email is required :src/modals/user.models.js"],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    refreshToken:{
+        type: String
     }
 })
 
